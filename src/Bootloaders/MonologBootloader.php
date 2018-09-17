17e36aa720739a489824208ad291abaf7c15019a
Wolfy-J <wolfy.jd@gmail.com>
2018-09-17 15:35:06 +0300

    injections and bootloader test

--- src/Bootloaders/MonologBootloader.php
+++ src/Bootloaders/MonologBootloader.php
@@ -6,19 +6,22 @@
  * @author    Anton Titov (Wolfy-J)
  */
 
-namespace Spiral\Logger;
+namespace Spiral\Logger\Bootloaders;
 
+use Monolog\Logger;
 use Psr\Log\LoggerInterface;
 use Spiral\Core\Bootloaders\Bootloader;
 use Spiral\Core\Container;
 use Spiral\Debug\LogsInterface;
+use Spiral\Logger\LogFactory;
 
 class MonologBootloader extends Bootloader
 {
     const BOOT = true;
 
     const SINGLETONS = [
-        LogsInterface::class => LogFactory::class
+        LogsInterface::class   => LogFactory::class,
+        LoggerInterface::class => Logger::class
     ];
 
     /**
@@ -26,6 +29,6 @@ class MonologBootloader extends Bootloader
      */
     public function boot(Container $container)
     {
-        $container->bindInjector(LoggerInterface::class, LogFactory::class);
+        $container->bindInjector(Logger::class, LogFactory::class);
     }
 }
--- tests/Logger/FactoryTest.php
+++ tests/Logger/FactoryTest.php
@@ -8,8 +8,12 @@
 
 namespace Spiral\Logger\Tests;
 
+use Monolog\Logger;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use Spiral\Core\BootloadManager;
 use Spiral\Core\Container;
+use Spiral\Logger\Bootloaders\MonologBootloader;
 use Spiral\Logger\Configs\MonologConfig;
 use Spiral\Logger\LogFactory;
 use Symfony\Component\EventDispatcher\EventDispatcher;
@@ -24,5 +28,18 @@ class FactoryTest extends TestCase
         $this->assertNotEmpty($logger);
         $this->assertSame($logger, $factory->getLogger());
         $this->assertSame($logger, $factory->getLogger(LogFactory::DEFAULT));
+    }
+
+    public function testInjection()
+    {
+        $factory = new LogFactory(new MonologConfig([]), new Container(), new EventDispatcher());
+        $logger = $factory->getLogger();
+
+        $container = new Container();
+        $container->get(BootloadManager::class)->bootload([MonologBootloader::class]);
+        $container->bind(LogFactory::class, $factory);
+
+        $this->assertSame($logger, $container->get(Logger::class));
+        $this->assertSame($logger, $container->get(LoggerInterface::class));
     }
 }
